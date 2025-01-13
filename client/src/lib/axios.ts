c1e8b1aed8ee71a6ff3db6e5f3da526518e2f998
Jonathon <jonp4208@gmail.com>
2025-01-12 21:05:04 -0500

    Update axios config to use relative URLs

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const PUBLIC_ROUTES = ['/api/login', '/api/register', '/api/auth/login', '/api/auth/register'];
 
 const api = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' ? 'https://cfa-eval-7eb74e14c3a4.herokuapp.com' : '',
+  baseURL: '',
   headers: {
     'Content-Type': 'application/json',
   },
