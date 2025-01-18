4691081b70db9725f80c2edd11ec00b24003e114
Jonathon <jonp4208@gmail.com>
2025-01-18 17:25:37 -0500

    feat: filter evaluations by manager relationship, add showAll option for admins

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -5,4 +5,5 @@ export { default as Evaluation } from './Evaluation.js';
 export { default as Template } from './Templates.js';
 export { default as Goal } from './Goal.js';
 export { default as Notification } from './Notification.js';
-export { default as Disciplinary } from './Disciplinary.js'; 
+export { default as Disciplinary } from './Disciplinary.js';
+export { default as GradingScale } from './GradingScale.js'; 
